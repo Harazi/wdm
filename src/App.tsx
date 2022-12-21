8f9316b18b84b4dc04fc55deff1df97feb17b373
husam <husam999h@gmail.com>
2022-12-21 18:41:05 +0300

    Moved the function to choose a download directory to GetDirHandle.

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,16 @@
 import React from "react"
-import GetDirHandle from "./GetDirHandle"
 import Interface from "./Interface"
+import LandingPage from "./landingPage"
 
+import type { Dispatch, SetStateAction } from "react"
 
 export default function App() {
 
   const [downloadDirHandle, setDownloadDirHandle] = React.useState<FileSystemDirectoryHandle | null>(null)
 
-  async function selectDownloadDirectory() {
-
-    try {
-      // Get the directory access
-      const dirHandle = await showDirectoryPicker({ startIn: "downloads" })
-
-      // Get read/write permissions
-      await dirHandle.getFileHandle(".wdm", { create: true })
-      await dirHandle.removeEntry(".wdm")
-
-      setDownloadDirHandle(dirHandle)
-    } catch (error) {
-      console.error(error)
-      setDownloadDirHandle(null)
-    }
-
-  }
-
   return downloadDirHandle
     ? <Interface downloadDirHandle={downloadDirHandle} />
-    : ("showDirectoryPicker" in window)
-    ? <GetDirHandle onClick={selectDownloadDirectory} />
-    : <p>
-        Your browser doesn't seem to support
-        {" "}
-        <a href="https://wicg.github.io/file-system-access/">File System Access</a>.
-        <br />
-        Use the latest version of
-        {" "}
-        <a href="https://www.google.com/chrome/">Chrome</a>
-        {" "}
-        to Access this website
-      </p>
+    : <LandingPage dirHandleSetter={setDownloadDirHandle} />
 }
+
+export type DirHandleSetter = Dispatch<SetStateAction<FileSystemDirectoryHandle | null>>
