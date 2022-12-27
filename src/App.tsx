8418bc99e0e934923df7c5c1e4e193d2684611d6
husam <husam999h@gmail.com>
2022-12-27 23:19:31 +0300

    Using src/utils/fs.ts for getting the directory handle

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,9 @@ import React from "react"
 import Interface from "./Interface"
 import LandingPage from "./landingPage"
 
-import type { Dispatch, SetStateAction } from "react"
-
 export default function App() {
 
-  const [downloadDirHandle, setDownloadDirHandle] = React.useState<FileSystemDirectoryHandle | null>(null)
-
-  return downloadDirHandle
-    ? <Interface downloadDirHandle={downloadDirHandle} />
-    : <LandingPage dirHandleSetter={setDownloadDirHandle} />
+  return "showDirectoryPicker" in window
+    ? <Interface />
+    : <LandingPage />
 }
-
-export type DirHandleSetter = Dispatch<SetStateAction<FileSystemDirectoryHandle | null>>
