b827ed7e17d3c457c12a20a86aa00343a0ea7cc7
husam <husam999h@gmail.com>
2023-01-02 01:42:08 +0300

    Removed all modal states from interface

--- src/Interface.tsx
+++ src/Interface.tsx
@@ -1,37 +1,17 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import Aside from "./aside/Aside"
 import Main from "./Main"
-import Modal from "./Modal"
 
 import type {
   DownloadEntryProperties,
-  MakeModalFunction,
-  CloseModalFunction,
   AddNewDownloadEntry,
   RemoveDownloadEntryFunction
 } from "./types"
 
-const modalElement = document.querySelector("#modal-root")
-
-if (!modalElement)
-  throw new Error("No modal element found!")
 
 export default function Interface() {
 
-  const [displayModal, setDisplayModal] = React.useState<React.ReactNode | null>(null)
-  const [modalTitle, setModalTitle] = React.useState("")
   const [downloadList, setDownloadList] = React.useState<DownloadEntryProperties[]>([])
-
-  const makeModal: MakeModalFunction = React.useCallback((component, title) => {
-    setDisplayModal(component)
-    setModalTitle(title)
-  }, [setDisplayModal, setModalTitle])
-
-  const closeModal: CloseModalFunction = React.useCallback(() => {
-    setDisplayModal(null)
-    setModalTitle("")
-  }, [setDisplayModal, setModalTitle])
 
   const addNewDownload: AddNewDownloadEntry = React.useCallback((url, fileName, parts, resumable, size) => {
     // TODO: save the download state in the list
@@ -58,17 +38,9 @@ export default function Interface() {
   return (
     <div className="main-container">
 
-      <Aside {...{ makeModal, addNewDownload }} />
+      <Aside addNewDownload={addNewDownload} />
 
       <Main {...{ downloadList, removeDownloadEntry }} />
-
-      {ReactDOM.createPortal(
-        <Modal
-          closeFn={closeModal}
-          title={modalTitle}
-          render={displayModal} />,
-        modalElement as Element // Already type guarded it up above
-      )}
 
     </div>
   )
