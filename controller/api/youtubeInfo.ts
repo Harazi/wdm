e4a1564e13bdf6030664b3f47be97bdd27a0aead
husam <husam999h@gmail.com>
2023-02-23 06:33:58 +0300

    Using the new youtube interfaces for type checking

--- controller/api/youtubeInfo.ts
+++ controller/api/youtubeInfo.ts
@@ -5,7 +5,13 @@ import type {
   Request,
   Response
 } from "express"
-import { YoutubeResponse } from "../types.js"
+import {
+  YoutubeResponse,
+  YTThumbnail,
+  YTSimpleFormat,
+  YTAdaptiveVideoFormat,
+  YTAdaptiveAudioFormat
+} from "../types.js"
 
 const YOUTUBE_API_URL = "https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
 
@@ -34,23 +40,24 @@ export default async function handler(req: Request, res: Response) {
   if (!isValidYoutubeResponse(youtubeRes))
     return res.status(502).send("Youtube's server send unkown response")
 
-  res.json({
+  const response: YoutubeResponse = {
+    videoDetails: {
+      videoId: youtubeRes.videoDetails.videoId,
+      title: youtubeRes.videoDetails.title,
+      lengthSeconds: youtubeRes.videoDetails.lengthSeconds,
+      thumbnail: {
+        thumbnails: youtubeRes.videoDetails.thumbnail.thumbnails
+      },
+      viewCount: youtubeRes.videoDetails.viewCount,
+      author: youtubeRes.videoDetails.author,
+    },
+    streamingData: {
+      formats: youtubeRes.streamingData.formats,
+      adaptiveFormats: youtubeRes.streamingData.adaptiveFormats,
+    },
+  }
 
-    author: youtubeRes.videoDetails.author,
-    title: youtubeRes.videoDetails.title,
-    lengthSeconds: youtubeRes.videoDetails.lengthSeconds,
-    viewCount: youtubeRes.videoDetails.viewCount,
-    thumbnail: youtubeRes.videoDetails.thumbnail.thumbnails[0],
-    // alias: f > formatObject
-    formats: youtubeRes.streamingData.formats.map(f => ({
-      mimeType: f.mimeType.split(";")[0],
-      extension: extension(f.mimeType.split(";")[0]),
-      qualityLabel: f.qualityLabel,
-      fps: f.fps,
-      url: f.url
-    }))
-
-  })
+  res.json({ success: true, data: response })
 
 }
 
@@ -67,24 +74,55 @@ const youtubePostData = (id: string) => ({
   },
 })
 
-const isValidYoutubeResponse = (obj: any): obj is YoutubeResponse => Boolean(
-
-  typeof obj?.videoDetails?.author === "string"
-    && typeof obj.videoDetails.lengthSeconds === "string"
+const isValidYoutubeResponse = (obj: any): obj is YoutubeResponse => {
+  return typeof obj?.videoDetails?.videoId === "string"
     && typeof obj.videoDetails.title === "string"
-    && typeof obj.videoDetails.viewCount === "string"
+    && typeof obj.videoDetails.lengthSeconds === "string"
+
     && Array.isArray(obj.videoDetails.thumbnail?.thumbnails)
-    && obj.videoDetails.thumbnail.thumbnails.every((thumbnail: any) => Boolean(
-      typeof thumbnail?.url === "string"
-        && typeof thumbnail.height === "number"
-        && typeof thumbnail.width === "number"
-    ))
+    && obj.videoDetails.thumbnail.thumbnails.every((thumbnail: any) => isYTThumbnail(thumbnail))
+
+    && typeof obj.videoDetails.viewCount === "string"
+    && typeof obj.videoDetails.author === "string"
 
     && Array.isArray(obj.streamingData?.formats)
-    && obj.streamingData.formats.every((format: any) => Boolean(
-      typeof format?.url === "string"
-        && typeof format.mimeType === "string"
-        && typeof format.qualityLabel === "string"
-        && typeof format.fps === "number"
-    ))
-)
+    && obj.streamingData.formats.every((format: any) => isYTSimpleFormat(format))
+
+    && Array.isArray(obj.streamingData?.adaptiveFormats)
+    && obj.streamingData.adaptiveFormats.every((format: any) => isYTAdaptiveVideoFormat(format) || isYTAdaptiveAudioFormat(format))
+}
+
+const isYTThumbnail = (obj: any): obj is YTThumbnail => {
+  return typeof obj?.url === "string"
+    && typeof obj.height === "number"
+    && typeof obj.width === "number"
+}
+
+const isYTSimpleFormat = (obj: any): obj is YTSimpleFormat => {
+  return typeof obj?.itag === "number"
+    && typeof obj.url === "string"
+    && typeof obj.mimeType === "string"
+    && typeof obj.width === "number"
+    && typeof obj.height === "number"
+    && typeof obj.fps === "number"
+    && typeof obj.qualityLabel === "string"
+    && typeof obj.bitrate === "number"
+}
+
+const isYTAdaptiveVideoFormat = (obj: any): obj is YTAdaptiveVideoFormat => {
+  return typeof obj?.itag === "number"
+    && typeof obj.url === "string"
+    && typeof obj.mimeType === "string"
+    && typeof obj.width === "number"
+    && typeof obj.height === "number"
+    && typeof obj.fps === "number"
+    && typeof obj.qualityLabel === "string"
+    && typeof obj.averageBitrate === "number"
+}
+
+const isYTAdaptiveAudioFormat = (obj: any): obj is YTAdaptiveAudioFormat => {
+  return typeof obj?.itag === "number"
+    && typeof obj.url === "string"
+    && typeof obj.mimeType === "string"
+    && typeof obj.averageBitrate === "number"
+}
