2d86de6a1fb62fc4c45618cd8171e2a89d435c78
husam <husam999h@gmail.com>
2023-03-04 17:06:49 +0300

    wrapped the App root with `DownloadListProvider`

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import Interface from "./Interface"
 import LandingPage from "./landingPage"
+import { DownloadListProvider } from "./contexts/DownloadListContext"
 
 export default function App() {
 
-  return "showDirectoryPicker" in window
-    ? <Interface />
-    : <LandingPage />
+  if (!Object.hasOwn(window, "showDirectoryPicker")) {
+    return <LandingPage />
+  }
+
+  return (
+    <DownloadListProvider>
+      <Interface />
+    </DownloadListProvider>
+  )
 }
