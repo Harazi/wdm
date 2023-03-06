66d88d9ba3fe9a38088cd696490c620ef8fa7d31
husam <husam999h@gmail.com>
2023-03-06 14:58:12 +0300

    Merged `fragmentsSizeMap` and `partsState` into `map`, generated by `setupMap` function, called only once behind `useMemo`.
    Removed `fragmentSize` as it can be computed from the `from` and `to`
    properties (if available).
    
    As the download progress, the component won't update. Instead it will be
    updated at interval (2 seconds) for performance.
    
    Storing the `downloadDirHandle` promise in a memo because it can't be
    accessed without user action.

--- src/components/DownloadEntry.tsx
+++ src/components/DownloadEntry.tsx
@@ -1,113 +1,84 @@
-import React from "react"
+import React, { useReducer, useContext, useMemo } from "react"
 import { divrem } from "divrem"
 import { streamToFile } from "../utils/streamToFile"
 import { dlDir } from "../utils/fs"
 import { DownloadSummary } from "./DownloadSummary"
+import { DownloadListContext } from "../contexts/DownloadListContext"
+import type { DownloadEntry } from "../contexts/DownloadListContext"
 
-import type {
-  DownloadEntryProperties,
-  RemoveDownloadEntryFunction
-} from "../types"
-
-interface DownloadEntryProps extends DownloadEntryProperties {
-  removeDownloadEntry: RemoveDownloadEntryFunction
+type fragment = {
+  readonly from?: number,
+  readonly to?: number,
+  downloaded: number
+  finished: boolean
 }
 
-export default function DownloadEntry({ id, url, fileName, parts, resumable, size, removeDownloadEntry }: DownloadEntryProps) {
+const setupMap = ({ size, resumable, parts }: { size?: number; resumable: boolean; parts: number }): fragment[] => {
+  if (!size || !resumable || parts === 1) return [{ downloaded: 0, finished: false }]
 
-  const startTime = React.useRef(Date.now())
-  const endTime = React.useRef<number | null>(null)
-  const totalDownloadSize = React.useRef(0)
+  const { count, rem } = divrem(size, parts)
+  let progress = 0
 
-  const [finishedDownloading, setFinishedDownloading] = React.useState(false)
-
-  const fragmentsSizeMap = React.useMemo(() => {
-
-    if (!resumable || !size) // Size is unknown, or server doesn't support range requests
-      return [{ fragmentSize: 0 }]
-
-    const { count, rem } = divrem(size, parts)
-
-    const map = Array(parts).fill(0).map(() => ({
-      fragmentSize: count,
-    }))
-
-    map[map.length - 1].fragmentSize += rem
-
-    return map
-  }, [])
-
-  const [partsState, setPartsState] = React.useState(Array(parts).fill(null).map(function fragmentMapFunction(v, i) {
-    return {
-      finished: false,
-      fragmentSize: fragmentsSizeMap[i].fragmentSize - 1,
-      startOffset: (fragmentsSizeMap[0].fragmentSize) * i,
-      downloadedBytes: 0,
-      index: i,
-    }
+  const map = Array(parts).fill(null).map(() => ({
+    from: progress,
+    to: (progress += count) - 1,
+    downloaded: 0,
+    finished: false,
   }))
 
+  map[map.length - 1].to += rem
+
+  return map
+}
+
+export default function DownloadEntry({ ID, url, fileName, size, resumable, parts }: DownloadEntry) {
+
+  const [, forceUpdate] = useReducer((p) => !p, false)
+  const startTime = useMemo(Date.now, [])
+  const endTime = React.useRef<number | null>(null)
+  const map = useMemo(() => setupMap({ size, resumable, parts }), [size, resumable, parts])
+  const [finished, setFinished] = React.useState(false)
+  const { remove } = useContext(DownloadListContext)
+  const dlDirP = useMemo(dlDir, [])
+  const intervalID = useMemo(() => setInterval(forceUpdate, 2000), [])
+
+  React.useEffect(() => () => clearInterval(intervalID), [])
   React.useEffect(() => {
 
     const abortController = new AbortController()
 
-    for (const fragment of partsState) {
+    map.forEach(async ({ from, to }, i) => {
+      const dirHandle = await dlDirP
+      const fileHandle = await dirHandle.getFileHandle(`${fileName}.part_${i}`, { create: true })
+      const writable = await fileHandle.createWritable({ keepExistingData: false })
 
-      (async () => {
+      const res = await fetch(`api/get?url=${encodeURIComponent(url.href)}`, {
+        signal: abortController.signal,
+        headers: from === undefined || to === undefined ? {} : {
+          range: `bytes=${from}-${to}`
+        }
+      })
 
-        const downloadDirHandle = await dlDir()
+      //TODO: Add a way to show errors to the user
+      if (!res.body) throw new Error("Response body is null!")
 
-        const fileWritable = await downloadDirHandle.getFileHandle(`${fileName}.part${fragment.index}`, { create: true })
-          .then(handle => handle.createWritable({ keepExistingData: false }))
+      const reader = res.body?.getReader()
 
-        const res = await fetch(`api/get?url=${encodeURIComponent(url.href)}`, {
-          redirect: "manual",
-          cache: "no-store",
-          referrer: "",
-          headers: {
-            "range": resumable
-              ? `bytes=${fragment.startOffset}-${fragment.startOffset + fragment.fragmentSize}`
-              : "bytes=0-"
-          }, //!! ????
-          signal: abortController.signal,
-        })
-
-        if (!res.body)
-          throw new Error("Response body is null!")
-
-        const reader = res.body.getReader()
-
-        streamToFile({
-          reader, writable: fileWritable,
-          on: {
-            progress: length => {
-              totalDownloadSize.current += length
-              setPartsState(parts => parts.map(fragmentObj => {
-
-                if (fragmentObj.index === fragment.index) {
-                  fragmentObj.downloadedBytes += length
-                }
-
-                return fragmentObj
-              }))
-            },
-            finish: async () => {
-              await fileWritable.close()
-              setPartsState(parts => parts.map(fragmentObj => {
-
-                if (fragmentObj.index === fragment.index) {
-                  fragmentObj.finished = true
-                }
-
-                return fragmentObj
-              }))
-            }
+      streamToFile({
+        reader,
+        writable,
+        on: {
+          progress: (length) => {
+            map[i].downloaded += length
+          },
+          finish: async () => {
+            await writable.close()
+            map[i].finished = true
+            forceUpdate()
           }
-        })
-
-      })()
-
-    }
+        }
+      })
+    })
 
     return () => {
       console.log("aborting right now")
@@ -116,70 +87,42 @@ export default function DownloadEntry({ id, url, fileName, parts, resumable, siz
   }, [])
 
   React.useEffect(() => {
+    // Initial run
+    if (!map.every(({ finished }) => finished)) return
 
-    console.log("partsState changed: inside useEffect")
+    (async () => {
+      const dirHandle = await dlDirP
 
-    const allFinishedDownloading = partsState.reduce((finished, fragment) => {
-      if (!finished) // At least one part did not finish
-        return false
-      else
-        return fragment.finished
-    }, true)
+      const writable = await dirHandle.getFileHandle(fileName, { create: true })
+        .then(handle => handle.createWritable({ keepExistingData: false }))
 
-    if (!allFinishedDownloading)
-      return
+      for (const index in map) {
+        const stream = await dirHandle.getFileHandle(`${fileName}.part_${index}`, { create: false })
+          .then(handle => handle.getFile())
+          .then(file => file.stream())
 
-    console.log("passed allFinishedDownloading test: inside useEffect")
+        await stream.pipeTo(writable, { preventClose: true })
+        await dirHandle.removeEntry(`${fileName}.part_${index}`)
+      }
 
-      ; (async () => {
+      await writable.close()
+      endTime.current = Date.now()
+      setFinished(true)
+      clearInterval(intervalID)
+    })()
 
-        const downloadDirHandle = await dlDir()
-
-        const fileWritable = await downloadDirHandle.getFileHandle(fileName, { create: true })
-          .then(handle => handle.createWritable({ keepExistingData: false }))
-
-        const promisesArray = partsState.map(async fragment => new Promise(async (resolve, reject) => {
-
-          const fragmentReader = await downloadDirHandle.getFileHandle(`${fileName}.part${fragment.index}`, { create: false })
-            .then(handle => handle.getFile())
-            .then(file => file.stream())
-            .then(stream => stream.getReader())
-
-          streamToFile({
-            reader: fragmentReader, writable: fileWritable,
-            on: {
-              finish: () => {
-                endTime.current = Date.now()
-                downloadDirHandle.removeEntry(`${fileName}.part${fragment.index}`) // Delete temporary part file
-                  .then(resolve)
-              }
-            }
-          })
-
-        }))
-
-        for (const fragmentWritePromise of promisesArray) {
-          await fragmentWritePromise
-        }
-
-        fileWritable.close()
-
-        setFinishedDownloading(true)
-
-      })()
-
-  }, [partsState])
+  }, [map.every(({ finished }) => finished)])
 
 
   return (
     <li>
 
-      <div className={`download-progress ${finishedDownloading && "finished"}`}>
-        {partsState.map(fragment => (
+      <div className={`download-progress ${finished && "finished"}`}>
+        {map.map(({ from, to, downloaded, finished }, index) => (
           <span
-            className={`fragment ${fragment.finished && "finished"}`}
-            style={{ "--fragment-download-progress": `${fragment.downloadedBytes / fragment.fragmentSize * 100}%` } as React.CSSProperties}
-            key={fragment.index}>
+            key={index}
+            className={`fragment ${finished && "finished"}`}
+            style={{ "--fragment-download-progress": size ? `${downloaded / ((to ?? size) - (from ?? 0)) * 100}%` : "" } as React.CSSProperties}>
           </span>))}
       </div>
 
@@ -191,9 +134,9 @@ export default function DownloadEntry({ id, url, fileName, parts, resumable, siz
         {url.href}
       </span>
 
-      {finishedDownloading &&
+      {finished &&
         (
-          <DownloadSummary time={((endTime.current ?? Date.now()) - startTime.current) / 1000} size={totalDownloadSize.current} />
+          <DownloadSummary time={((endTime.current ?? Date.now()) - startTime) / 1000} size={map.reduce((acc, { downloaded }) => acc + downloaded, 0)} />
         ) ||
         (
           <div className="controll-buttons">
@@ -205,7 +148,7 @@ export default function DownloadEntry({ id, url, fileName, parts, resumable, siz
               </button>
             )}
 
-            <button type="button" className="cancel" onClick={() => removeDownloadEntry(id)}>
+            <button type="button" className="cancel" onClick={() => remove(ID)}>
               <img src="icons/delete-forever_white_hq_18dp.png" alt="Delete Icon" />
               Cancel
             </button>
